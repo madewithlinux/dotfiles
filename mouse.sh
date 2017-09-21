0260f158d7560a1c9481804c07d669a0c0d019b8
Josh Wright <madewithlinux.josh@gmail.com>
2017-09-21 18:09:40 -0500

    add mouse remap script

---
+++ mouse.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mouse_id="$(xinput | awk -F'[=]' '/Razer Orochi/ {print $2}' | awk '{print $1}' |tail -n 1)"
+
+xinput set-prop $mouse_id 'Device Accel Constant Deceleration' 3
+xinput set-button-map $mouse_id  3 2 1 4 5 3 3 3 3
