a98fe3f4768fe9965d87fc41ba0d180d62d69884
Josh Wright <madewithlinux.josh@gmail.com>
2017-12-21 16:40:35 -0600

    shorter git-info

--- src/bash/git-info.sh
+++ src/bash/git-info.sh
@@ -7,11 +7,17 @@ info() {
 	git diff HEAD | cat
 }
 
-info
-# in tests, gzip has best compression for these small text snippets
-echo "############################# gzip -9 | base64 #############################"
-info | gzip -9 | base64
-# echo "############################# bzip2 -9 | base64 ############################"
-# info | bzip2 -9 | base64
-# echo "############################# xz -9 | base64 ###############################"
-# info | xz -9 | base64
+if [[ "$1" == "short" ]]; then
+	echo "base64 -d << EOF | gunzip"
+	info | gzip -9 | base64
+	echo "EOF"
+else
+	info
+	# in tests, gzip has best compression for these small text snippets
+	echo "############################# gzip -9 | base64 #############################"
+	info | gzip -9 | base64
+	# echo "############################# bzip2 -9 | base64 ############################"
+	# info | bzip2 -9 | base64
+	# echo "############################# xz -9 | base64 ###############################"
+	# info | xz -9 | base64
+fi
