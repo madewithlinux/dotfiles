3a31c75ce825f23f55e828b89d02cbaede2af4ef
Josh Wright <madewithlinux.josh@gmail.com>
2017-12-22 18:27:22 -0600

    slightly change git-info.sh, be more consistent

--- src/bash/git-info.sh
+++ src/bash/git-info.sh
@@ -3,7 +3,7 @@
 info() {
 	echo "origin: $(git config --get remote.origin.url)"
 	echo "HEAD: $(git rev-parse HEAD)"
-	# echo "diff:"
+	# pipe to cat to make sure it doesn't open pager
 	git diff HEAD | cat
 }
 
@@ -14,8 +14,9 @@ if [[ "$1" == "short" ]]; then
 else
 	info
 	# in tests, gzip has best compression for these small text snippets
-	echo "############################# gzip -9 | base64 #############################"
+	echo "base64 -d << EOF | gunzip"
 	info | gzip -9 | base64
+	echo "EOF"
 	# echo "############################# bzip2 -9 | base64 ############################"
 	# info | bzip2 -9 | base64
 	# echo "############################# xz -9 | base64 ###############################"
