57c68cdeedca5aa998a18cb69d3beb6bd7704000
Josh Wright <madewithlinux.josh@gmail.com>
2019-12-18 21:49:59 -0600

    add dynalist notes parser thing

---
+++ src/python/dynalist-notes-grep.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# finds all files below CWD, recursively, 
+
+import sys, os
+
+def countTabs(line: str) -> int:
+    for i in range(len(line)):
+        if line[i] != '\t':
+            return i
+    return 0
+
+def printLines(filenamePathPrefix: str, filename: str):
+    linesPath = [filename]
+    runningIndent = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            thisLineIndent = countTabs(line)
+            line = line.strip()
+            if thisLineIndent > runningIndent:
+                linesPath.append(line)
+                print(filenamePathPrefix + ' |> '.join(linesPath))
+                runningIndent = thisLineIndent
+            elif thisLineIndent == runningIndent:
+                linesPath[-1] = line
+                print(filenamePathPrefix + ' |> '.join(linesPath))
+            else: # thisLineIndent < runningIndent
+                indentsToRemove = runningIndent - thisLineIndent
+                linesPath = linesPath[0:len(linesPath) - 1 - indentsToRemove]
+                linesPath.append(line)
+                print(filenamePathPrefix + ' |> '.join(linesPath))
+                runningIndent = thisLineIndent
+
+
+for dirpath, dirnames, filenames in os.walk('.'):
+    print(dirpath, dirnames, filenames)
+    for filename in filenames:
+        if not filename.endswith('.txt'):
+            continue
+        filenamePath = os.path.join(dirpath, filename)
+        printLines(filenamePath + ':', filenamePath)
+
