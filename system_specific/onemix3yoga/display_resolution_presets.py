3d64f31bd872fc9024248197291c88434c840bf6
Josh <madewithlinux.josh@gmail.com>
2020-01-11 15:35:03 -0600

    more resolutions

--- system_specific/onemix3yoga/display_resolution_presets.py
+++ system_specific/onemix3yoga/display_resolution_presets.py
@@ -54,6 +54,7 @@ def writePreset(name: str,
     os.chmod(scriptPath, os.stat(scriptPath).st_mode | stat.S_IEXEC)
 
     desktopLauncherPath = os.path.join(desktopLauncherBasePath, file_name+'.desktop')
+    print(desktopLauncherPath)
     with open(desktopLauncherPath, 'w') as f:
         f.write(f"""[Desktop Entry]
 Version=1.1
@@ -74,6 +75,40 @@ writePreset(
             'auto': True,
             'scale': 1,
             'rotate': 'left',
+        },
+        'DP-1': { 'enabled': False },
+        'HDMI-1': { 'enabled': False },
+    },
+    uiScale=2)
+
+def downscaledMode(a,b):
+    writePreset(
+        f'internal display only {a}',
+        xrandr={
+            'eDP-1': {
+                'enabled': True,
+                'auto': False,
+                'mode': b,
+                'scale': 1,
+                'rotate': 'left',
+            },
+            'DP-1': { 'enabled': False },
+            'HDMI-1': { 'enabled': False },
+        },
+        uiScale=1)
+downscaledMode('1280x720', '720x1280_60.00')
+downscaledMode('1280x800', '800x1280_60.00')
+downscaledMode('1920x1080', '1080x1920_60.00')
+downscaledMode('2560x1440', '1440x2560_60.00')
+
+writePreset(
+    'internal display only right',
+    xrandr={
+        'eDP-1': {
+            'enabled': True,
+            'auto': True,
+            'scale': 1,
+            'rotate': 'normal',
         },
         'DP-1': { 'enabled': False },
         'HDMI-1': { 'enabled': False },
@@ -104,6 +139,21 @@ writePreset(
             'scale': 1,
         },
         'HDMI-1': { 'enabled': False },
+    },
+    uiScale=2,
+)
+
+
+writePreset(
+    'HDMI-1 only 1080p scale=2',
+    xrandr={
+        'eDP-1': { 'enabled': False },
+        'HDMI-1': {
+            'enabled': True,
+            'mode': '1920x1080',
+            'scale': 1,
+        },
+        'DP-1': { 'enabled': False },
     },
     uiScale=2,
 )
