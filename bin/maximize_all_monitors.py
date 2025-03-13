edb14562c2cc5f6111275e5c495fda301b185462
Julia <6527677+madewithlinux@users.noreply.github.com>
2025-03-13 15:16:57 -0600

    add ugly hack to maximize firefox across both monitors

--- bin/maximize_all_monitors.py
+++ bin/maximize_all_monitors.py
@@ -6,8 +6,11 @@ import sys
 
 # Get active/focused window
 out = subprocess.check_output(['xprop', '-root', '_NET_ACTIVE_WINDOW']).decode('ascii', 'ignore')
+# print(out)
 list_id = [re.search("window id # (0x[0-9a-f]+)", out).group(1)]
 id = list_id[0]
+
+wm_class_ret = subprocess.check_output(['xprop', '-id', id, 'WM_CLASS']).decode('ascii', 'ignore')
 
 
 # Get screens information and order them
@@ -33,12 +36,20 @@ full_height = 2880
 
 # TODO: find a way to determine these values programmatically
 taskbar_buffer = 0
+x = 0
+y = 0
 margin_x = 10
 margin_y = 98
+
+if "firefox_firefox" in wm_class_ret:
+	# ugly hack to fix firefox
+	x = -50
+	margin_x = -100
+	margin_y = -49
 
 full_width -= margin_x
 full_height -= margin_y
 # move and resize the window
-cmd = ['wmctrl', '-i', '-r', id, '-e', f'0,0,{taskbar_buffer},{full_width},{full_height-taskbar_buffer}']
+cmd = ['wmctrl', '-i', '-r', id, '-e', f'0,{x},{y+taskbar_buffer},{full_width},{full_height-taskbar_buffer}']
 print(f'{cmd=}')
 subprocess.call(cmd)
